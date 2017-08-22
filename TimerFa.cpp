54d60f76bc8b1e3e4acee12343c184d17f59067f
Fatih Aşlamacı <fatihaslamaci@gmail.com>
2017-08-22 15:49:29 +0300

    Update TimerFa.cpp
    micros overflow proplem

--- TimerFa.cpp
+++ TimerFa.cpp
@@ -1,28 +1,34 @@
 #include <Arduino.h>
 #include "TimerFa.h"
+#include <limits.h>
 
 
 bool TimerFa::MicrosaniyedeBir(unsigned long BeklemeSuresi)
 {
   volatile bool R = false;
   volatile unsigned long now = micros();
-  volatile unsigned long fark = now - BeklemeSuresi;
   if (SonOkunanMicrosaniye > now)
   {
+    ArtikSure=ULONG_MAX - SonOkunanMicrosaniye;
     SonOkunanMicrosaniye = 0;
   }
+  volatile unsigned long fark = now - BeklemeSuresi-ArtikSure;
   if ((SonOkunanMicrosaniye < fark) && (BeklemeSuresi < now))
   {
-    SonOkunanMicrosaniye = now - (now - SonOkunanMicrosaniye - BeklemeSuresi);
+    SonOkunanMicrosaniye = now - (now - SonOkunanMicrosaniye - BeklemeSuresi)-ArtikSure;
+    ArtikSure=0;
     R = true;
   }
   return R;
-  
 }
 
 void TimerFa::Reset()
 {
     SonOkunanMicrosaniye=micros();
+    ArtikSure=0;
 }
+
+
+
 
 
